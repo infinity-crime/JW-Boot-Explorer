05facbe2305af40b24da299895e3724a3ce3d306
Kirill Zhestkov <infinity.crime@outlook.com>
2024-10-28 22:36:31 +0300

    Убран лишний мусор

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace JW_Boot_Explorer
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
